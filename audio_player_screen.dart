aa1bd36caee586cbb91b611692b9db953017c18a
vish0420 <vignesh11806@gmail.com>
2026-08-14 10:41:51 +0530

    Correct audio player model import

--- audio_player_screen.dart
+++ audio_player_screen.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:just_audio/just_audio.dart';
 import 'package:just_audio_background/just_audio_background.dart';
 
-import 'models/audio_item.dart';
+import '../models/audio_item.dart';
 
 class AudioPlayerScreen extends StatefulWidget {
   final AudioItem audio;
